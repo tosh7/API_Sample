1f3f2c5b12724bd0d87497e2b7d31974c00536fc
インターン4 dev室 <dev_intern4@administratornomac-mini-2.local>
2018-11-09 11:39:38 +0900

    Initial Commit

---
+++ API-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  API-Sample
+//
+//  Created by dev室 インターン4 on 2018/11/09.
+//  Copyright © 2018年 dev室 インターン4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
